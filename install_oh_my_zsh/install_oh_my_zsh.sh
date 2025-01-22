f42045e2b3b346e2b4096c8e7da1bab3d9d37954
王哼哼 <wangyuanchuang@qq.com>
2025-01-22 11:55:58 +0800

    Update install_oh_my_zsh.sh

--- install_oh_my_zsh/install_oh_my_zsh.sh
+++ install_oh_my_zsh/install_oh_my_zsh.sh
@@ -7,7 +7,14 @@ wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - 
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-sed -i "s/plugins=(/plugins=(zsh-autosuggestions zsh-syntax-highlighting zsh-completions /" ~/.zshrc
+
+SED_CMD="sed"
+SED_ARGS="-i"
+# 在 macOS 上，sed -i 的语法略有不同，需要添加一个额外的空字符串作为后缀，即 sed -i ''
+if [[ "$(uname)" == "Darwin" ]]; then
+    SED_ARGS="$SED_ARGS ''"
+fi
+$SED_CMD $SED_ARGS "s/plugins=(/plugins=(zsh-autosuggestions zsh-syntax-highlighting zsh-completions /" ~/.zshrc
 
 wget https://github.com/ahmetb/kubectl-aliases/raw/master/.kubectl_aliases -O ~/.kubectl_aliases
 
