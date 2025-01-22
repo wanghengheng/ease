7a13195a3850ce0bfd52dd00e3eac99055acb103
王哼哼 <wangyuanchuang@qq.com>
2025-01-22 14:48:34 +0800

    Update install_oh_my_zsh.sh

--- install_oh_my_zsh/install_oh_my_zsh.sh
+++ install_oh_my_zsh/install_oh_my_zsh.sh
@@ -27,6 +27,23 @@ EOF
 
 source ~/.zshrc
 
-chsh -s /bin/zsh
+
+if ! command -v chsh > /dev/null 2>&1; then
+    # 对于 CentOS 或其他基于 RPM 的系统
+    if [[ -f /etc/redhat-release ]]; then
+        yum install -y util-linux-user
+    # 对于 Ubuntu 或其他基于 DEB 的系统
+    elif [[ -f /etc/debian_version ]]; then
+        apt-get update
+        apt-get install -y passwd
+    # 对于其他系统，可以根据需要添加更多的检查和安装逻辑
+    else
+        echo "Unsupported Linux distribution. Please install chsh manually."
+    fi
+fi
+
+if command -v chsh > /dev/null 2>&1; then
+    chsh -s /bin/zsh
+fi
 
 zsh
